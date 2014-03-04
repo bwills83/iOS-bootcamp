3e032ab2bd3a5e87699640ed3f7c11af17ff981f
Vanessa Grass <vnesag@gmail.com>
2014-03-04 13:42:02 -0800

    added tip calculator
    yahoo

---
+++ tipcalculator/tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Vanessa Grass on 3/4/14.
+//  Copyright (c) 2014 Vanessa Grass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
